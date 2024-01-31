5be27e9f377512d8724577409fed60f5b27b0d02
Matt C <matthew.compton@mymail.champlain.edu>
2024-01-31 18:55:52 -0500

    epel for sanity

--- install-horizon.sh
+++ install-horizon.sh
@@ -2,7 +2,7 @@
 
 sudo setenforce 0
 
-sudo yum -y install wget nano
+sudo yum -y install wget nano epel-release
 
 # Install PSQL Repo
 sudo yum -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
