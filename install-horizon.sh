0481dd3d422697cf19f25a2b156ed3f0a94fa3b4
Matt C <matthew.compton@mymail.champlain.edu>
2024-02-04 10:33:35 -0500

    Update install-horizon.sh

--- install-horizon.sh
+++ install-horizon.sh
@@ -3,11 +3,15 @@
 printf "Database password: "
 read psqlroot
 
+# Goodbye SELinux
 sudo setenforce 0
 
+# Install random useful packages
+# EPEL is updated packages for older RPM distros
+# (like Cent 7)
 sudo yum -y install wget nano epel-release
 
-# Install PSQL Repo
+# Install PSQL's package repo
 sudo yum -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 
 # Install PSQL
@@ -19,34 +23,37 @@ sudo /usr/pgsql-12/bin/postgresql-12-setup initdb
 # Enable psql
 sudo systemctl enable --now postgresql-12
 
-# user for opennms
+# PSQL user for opennms
 sudo -i -u postgres createuser opennms
 
-# make database
+# make psql database for openmnms
 sudo -i -u postgres createdb -O opennms opennms
 
 # Set SQL superuser password
 sudo -i -u postgres psql -c "ALTER USER postgres WITH PASSWORD '${psqlroot}';"
 sudo -i -u postgres psql -c "ALTER USER opennms WITH PASSWORD '${psqlroot}';"
 
+# Manually overwrite the file that controls access to postgres
+# In our specific case, this allows localhost connections
+# with md5 hashed passwords.
 sudo cp pg_hba.conf /var/lib/pgsql/12/data/.
 sudo chown postgres:postgres /var/lib/pgsql/12/data/pg_hba.conf
 sudo chmod 600 /var/lib/pgsql/12/data/pg_hba.conf
 
-# restart
+# restart the postgres service
 sudo systemctl reload postgresql-12
 
-# Download Java
+# Download Java package
 wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm
 
-# Install Java
+# Install Java package
 sudo yum -y install ./jdk-17_linux-x64_bin.rpm
 
-# Install OpenNMS repo & gpg key
+# Install OpenNMS repo & gpg key (this is straight from their docs)
 sudo yum -y install https://yum.opennms.org/repofiles/opennms-repo-stable-rhel7.noarch.rpm
 sudo rpm --import https://yum.opennms.org/OPENNMS-GPG-KEY
 
-# Install the things
+# Install the opennms packages (this is straight from their docs)
 sudo yum -y install rrdtool jrrd2 jicmp jicmp6 opennms-core opennms-webapp-jetty opennms-plugin-cloud opennms-webapp-hawtio
 
 # Add the OpenNMS psql user password
@@ -55,21 +62,22 @@ sudo -u opennms /opt/opennms/bin/scvcli set postgres opennms $psqlroot
 # Config the psql root user's password
 sudo -u opennms /opt/opennms/bin/scvcli set postgres-admin postgres $psqlroot
 
-# Replace creds for users
-# sudo -u opennms nano /opt/opennms/etc/opennms-datasources.xml
+# Replace creds for users (this is straight from their docs)
+# (The documented way to do this is manually editing this file,
+# but that's not very clean for an automated install)
 sudo cp opennms-datasources.xml /opt/opennms/etc/.
 sudo chown opennms:opennms /opt/opennms/etc/opennms-datasources.xml
 sudo chmod 664 vi /opt/opennms/etc/opennms-datasources.xml
 
-# Autodetect best java
+# Autodetect best java runtime for opennms (this is straight from their docs)
 sudo /opt/opennms/bin/runjava -s
 
-# Init database
+# Init database (this is straight from their docs)
 sudo /opt/opennms/bin/install -dis
 
 # Start and enable service
 sudo systemctl enable --now opennms
 
-# Open port
+# Open port thru firewall
 sudo firewall-cmd --permanent --add-port=8980/tcp
 sudo firewall-cmd --reload
